0c9b6e0efda5bf1820bb94dde9564b6c51abae7b
ryampeguero <ryampeguero@gmail.com>
2024-04-05 11:33:02 +0200

    fix: array name changed to one who's more clear

--- jsnack/script.js
+++ jsnack/script.js
@@ -3,14 +3,15 @@
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
 // Ogni volta che ne crei uno, stampalo a schermo.
 const N = parseInt(prompt("Quanti array vuoi?"));
-const array = [];
+const arrayContainer = [];
+
 for (let i = 0; i < N; i++) {
-    array[i] = newArray();
+    arrayContainer[i] = newArray();
     for (let j = 0; j < 100; j++) {
-        array[i][j] = rndNumb(1,100);
+        arrayContainer[i][j] = rndNumb(1,100);
         
     }
-    console.log(array[i]);
+    console.log(arrayContainer[i]);
 }
 
 console.log(array);
