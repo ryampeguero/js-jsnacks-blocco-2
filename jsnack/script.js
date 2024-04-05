edf93057df62a8ad7b671cb2e098b7342cec2292
ryampeguero <ryampeguero@gmail.com>
2024-04-05 11:45:00 +0200

    feat: added array to html file

--- jsnack/script.js
+++ jsnack/script.js
@@ -4,10 +4,14 @@
 // Ogni volta che ne crei uno, stampalo a schermo.
 const N = parseInt(prompt("Quanti array vuoi?"));
 const arrayContainer = [];
+const arrayList = document.createElement("ul");
+
 
 for (let i = 0; i < N; i++) {
     arrayContainer[i] = newArray(100);
+    arrayList.innerHTML += `<li>${i, arrayContainer[i]}</li>`; 
     console.log(arrayContainer[i]);
 }
 
+document.querySelector("body").append(arrayList);
 console.log(arrayContainer);
