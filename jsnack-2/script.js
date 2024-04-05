3885a1a708b44dc82f936377abf649a51d248287
ryampeguero <ryampeguero@gmail.com>
2024-04-05 12:34:36 +0200

    feat: added word

--- jsnack-2/script.js
+++ jsnack-2/script.js
@@ -3,7 +3,14 @@
 // Dare la possibilità di inserire due parole.
 // Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
-const parola1 = prompt("Inserisci una parola");
-const parola2 = prompt("Inserisci un'altra parola");
+let parola1;
+let parola2;
+do{
+    parola1 = prompt("Inserisci una parola");
+    parola2 = prompt("Inserisci un'altra parola");
+
+
+
+}while(parola1 =="" || parola1 ==null || parola2 =="" || parola2==null);
 
 compare2Words(parola1, parola2);
